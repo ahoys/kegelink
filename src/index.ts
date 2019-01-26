18c65d3d59a17dcdf515791be6a8110aceb18f5f
Ari Höysniemi <ari.hoysniemi@gmail.com>
2019-01-26 14:20:22 +0200

    Added everything needed for building.

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import logscribe from 'logscribe';
+
+const { l } = logscribe('General');
+
+l('moi');
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+// Root paths.
+const root = path.resolve(__dirname, './')
+const build = path.resolve(__dirname, './build')
+const src = path.resolve(__dirname, './src')
+
+module.exports = {
+  mode: 'production',
+  target: 'node',
+  entry: `${src}/index.ts`,
+  output: {
+    path: build,
+    filename: 'index.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+    modules: ['node_modules'],
+  },
+  plugins: [
+    // Cleans the destination folder before building new.
+    new CleanWebpackPlugin([build], { root }),
+  ],
+}
